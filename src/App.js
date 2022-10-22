4d8e07dc03865142b2dd04d0557b223e4dbe7f2c
Kiera Thomas <kiera.c.thomas@vanderbilt.edu>
2022-10-22 06:53:28 -0500

    css styling for popupbutton

--- src/App.js
+++ src/App.js
@@ -1080,18 +1080,7 @@ function App() {
                           <div className="restaurantTitle">
                           <b> {restaurant.name} </b>
                           </div>
-                          <p>
-                            Lorem ipsum dolor sit amet, consectetur adipiscing
-                            elit, sed do eiusmod tempor incididunt ut labore et
-                            dolore magna aliqua. Ut enim ad minim veniam, quis
-                            nostrud exercitation ullamco laboris nisi ut aliquip
-                            ex ea commodo consequat. Duis aute irure dolor in
-                            reprehenderit in voluptate velit esse cillum dolore
-                            eu fugiat nulla pariatur. Excepteur sint occaecat
-                            cupidatat non proident, sunt in culpa qui officia
-                            deserunt mollit anim id est laborum.
-                          </p>
-                          <button>Test button</button>
+                          <button className="popupButton"> Text Me Directions! </button>
                         </>
                       }
                       handleClose={togglePopup}
