2b862fad253bb46b51b5bf4c25f90ffb056dd5a7
Kiera Thomas <kiera.c.thomas@vanderbilt.edu>
2022-10-22 05:52:48 -0500

    added popups

--- src/App.js
+++ src/App.js
@@ -896,6 +896,11 @@ const categoryOptions = [
 const sortOptions = [{ value: "price", label: "price" }];
 
 function App() {
+  const [isOpen, setIsOpen] = useState(false);
+ 
+  const togglePopup = () => {
+    setIsOpen(!isOpen);
+  }
   //state for categories
   const [selectedCategory, setSelectedCategory] = useState(null);
   //state for sort by
@@ -951,9 +956,25 @@ function App() {
           (restaurant.isGetApp && selectedCategory == "get")
         )
           return (
+            <>
+            <input
+      type="button"
+      value="Click to Open Popup"
+      onClick={togglePopup}
+    />
+    <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.</p>
+    {isOpen && <Popup
+      content={<>
+        <b>Design your Popup</b>
+        <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.</p>
+        <button>Test button</button>
+      </>}
+      handleClose={togglePopup}
+    />}
             <div id="boxes">
               <Restaurant name={restaurant.name} price={restaurant.price} />
             </div>
+            </>
           );
       })}
     </div>
