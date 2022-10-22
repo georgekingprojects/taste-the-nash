dafdf93383cc9804231d485203a7782b22d0e9de
George King <george.y.wang@vanderbilt.edu>
2022-10-22 00:53:25 -0500

    basic template

--- src/App.js
+++ src/App.js
@@ -1,23 +1,12 @@
-import logo from './logo.svg';
-import './App.css';
+import logo from "./logo.svg";
+import "./App.css";
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <h1>Taste the Nash!</h1>
+      <h3>Sarah put map stuff here</h3>
+      <h4>We will put search and restaurant component stuff here.</h4>
     </div>
   );
 }
