1698feec09ce95be857e127ca6ba5bdca65ee332
Kiera Thomas <kiera.c.thomas@vanderbilt.edu>
2022-10-22 16:44:38 -0500

    added nav bar hamburger menu

--- src/App.js
+++ src/App.js
@@ -1094,6 +1094,21 @@ function App() {
 
   return (
     <div className="App">
+
+<div class="hamburger-menu">
+    <input id="menu__toggle" type="checkbox" />
+    <label class="menu__btn" for="menu__toggle">
+      <span></span>
+    </label>
+
+    <ul class="menu__box">
+      <li><a class="menu__item" href="#">Home</a></li>
+      <li><a class="menu__item" href="#">About</a></li>
+      <li><a class="menu__item" href="#">Team</a></li>
+      <li><a class="menu__item" href="#">Contact</a></li>
+      <li><a class="menu__item" href="#">Twitter</a></li>
+    </ul>
+  </div>
       {/*
       <div id="nav">
         <a href="#map" class="navlink">
