91dce5360dbc30ec15571931816a205629a4de33
George King <george.y.wang@vanderbilt.edu>
2022-10-22 23:42:20 -0500

    changes

--- src/Restaurant.js
+++ src/Restaurant.js
@@ -109,6 +109,7 @@ function Restaurant(props) {
             flexDirection: "row",
             alignItems: "center",
             justifyContent: "space-evenly",
+            marginTop: "-15px",
           }}
         >
           <img
@@ -216,117 +217,122 @@ function Restaurant(props) {
                 : "None"
             }
           />
-          <h3>{props.name}</h3>
+          <h3>
+            {props.name}
+            <br />
+            {d && <p style={{ fontSize: 12 }}>Distance: {d} miles</p>}
+          </h3>
         </div>
-        {d && <p>Distance: {d} miles</p>}
       </div>
 
       {isOpen && (
         <Popup
           content={
             <>
-            <img class ="popupImg"
-            src={
-              props.name == "The Ainsworth"
-                ? require("./imgfiles/ainsworth.png")
-                : props.name == "Anzie Blue"
-                ? require("./imgfiles/anzieblue.png")
-                : props.name == "Banh Mi and Roll+"
-                ? require("./imgfiles/banhmiroll.png")
-                : props.name == "Barista Parlor"
-                ? require("./imgfiles/baristaparlor.jpg")
-                : props.name == "Biscuit Love"
-                ? require("./imgfiles/biscuitlove.png")
-                : props.name == "Bombay Palace"
-                ? require("./imgfiles/bombaypalace.png")
-                : props.name == "Cabana Taps"
-                ? require("./imgfiles/cabanataps.png")
-                : props.name == "Cafe Coco"
-                ? require("./imgfiles/cafecoco.png")
-                : props.name == "Central BBQ"
-                ? require("./imgfiles/centralbbq.png")
-                : props.name == "Chili's Grill & Bar"
-                ? require("./imgfiles/chilis.png")
-                : props.name == "Chuy's"
-                ? require("./imgfiles/chuys.png")
-                : props.name == "Crab Fever"
-                ? require("./imgfiles/crabfever.png")
-                : props.name == "Donato's Pizza"
-                ? require("./imgfiles/donatos.png")
-                : props.name == "Elliston Place Soda Shop"
-                ? require("./imgfiles/ellistonplace.png")
-                : props.name == "Fable Lounge"
-                ? require("./imgfiles/fable.png")
-                : props.name == "Frutta Bowls"
-                ? require("./imgfiles/fruttabowl.png")
-                : props.name == "Grain & Berry"
-                ? require("./imgfiles/grainandberry.png")
-                : props.name == "The Grilled Cheeserie"
-                ? require("./imgfiles/grilledcheeserie.png")
-                : props.name == "Helen's Hot Chicken"
-                ? require("./imgfiles/helenshotchicken.png")
-                : props.name == "Holy Smokes"
-                ? require("./imgfiles/holysmokes.png")
-                : props.name == "Hopdoddy Burger Bar"
-                ? require("./imgfiles/hopdoddy.png")
-                : props.name == "I Love Sushi"
-                ? require("./imgfiles/ilovesushi.png")
-                : props.name == "Inchin's Bamboo Garden"
-                ? require("./imgfiles/inchins.png")
-                : props.name == "Jamba"
-                ? require("./imgfiles/jamba.png")
-                : props.name == "Jeni's Splendid Ice Creams"
-                ? require("./imgfiles/jenis.png")
-                : props.name == "Jet's Pizza"
-                ? require("./imgfiles/jetspizza.png")
-                : props.name == "JP Cafe"
-                ? require("./imgfiles/jpcafe.png")
-                : props.name == "Koi Sushi & Thai"
-                ? require("./imgfiles/koisushi.jpeg")
-                : props.name == "JP Cafe"
-                ? require("./imgfiles/jpcafe.png")
-                : props.name == "Meet Noodles"
-                ? require("./imgfiles/meetnoodles.png")
-                : props.name == "Mellow Mushroom"
-                ? require("./imgfiles/mellowmushroom.png")
-                : props.name == "Michaelangelo's Pizza"
-                ? require("./imgfiles/michaelangelo.png")
-                : props.name == "Nada"
-                ? require("./imgfiles/nada.png")
-                : props.name == "Nicoletto's Italian Kitchen"
-                ? require("./imgfiles/nicolettos.png")
-                : props.name == "Oscar's Taco Shop"
-                ? require("./imgfiles/oscarstacoshop.png")
-                : props.name == "Papa John's Pizza"
-                ? require("./imgfiles/papajohns.png")
-                : props.name == "Poke Bros"
-                ? require("./imgfiles/pokebros.png")
-                : props.name == "Roma Pizza & Pasta"
-                ? require("./imgfiles/romapizza.png")
-                : props.name == "Sarabha's Creamery"
-                ? require("./imgfiles/sarabhas.png")
-                : props.name == "Satay Thai Grill"
-                ? require("./imgfiles/satay.png")
-                : props.name == "Shokku Ramen"
-                ? require("./imgfiles/shokkuramen.png")
-                : props.name == "Sitar Indian Restaurant"
-                ? require("./imgfiles/sitar.png")
-                : props.name == "The Slider House"
-                ? require("./imgfiles/sliderhouse.png")
-                : props.name == "Sun & Fork"
-                ? require("./imgfiles/sunandfork.png")
-                : props.name == "Sweet Dots"
-                ? require("./imgfiles/sweetdots.png")
-                : props.name == "Taco Mama"
-                ? require("./imgfiles/tacomama.png")
-                : props.name == "Urban Cookhouse"
-                ? require("./imgfiles/urbancookhouse.png")
-                : props.name == "The Urban Juicer"
-                ? require("./imgfiles/urbanjuicer.png")
-                : props.name == "Wendy's"
-                ? require("./imgfiles/wendys.png")
-                : "None"
-            }/>
+              <img
+                class="popupImg"
+                src={
+                  props.name == "The Ainsworth"
+                    ? require("./imgfiles/ainsworth.png")
+                    : props.name == "Anzie Blue"
+                    ? require("./imgfiles/anzieblue.png")
+                    : props.name == "Banh Mi and Roll+"
+                    ? require("./imgfiles/banhmiroll.png")
+                    : props.name == "Barista Parlor"
+                    ? require("./imgfiles/baristaparlor.jpg")
+                    : props.name == "Biscuit Love"
+                    ? require("./imgfiles/biscuitlove.png")
+                    : props.name == "Bombay Palace"
+                    ? require("./imgfiles/bombaypalace.png")
+                    : props.name == "Cabana Taps"
+                    ? require("./imgfiles/cabanataps.png")
+                    : props.name == "Cafe Coco"
+                    ? require("./imgfiles/cafecoco.png")
+                    : props.name == "Central BBQ"
+                    ? require("./imgfiles/centralbbq.png")
+                    : props.name == "Chili's Grill & Bar"
+                    ? require("./imgfiles/chilis.png")
+                    : props.name == "Chuy's"
+                    ? require("./imgfiles/chuys.png")
+                    : props.name == "Crab Fever"
+                    ? require("./imgfiles/crabfever.png")
+                    : props.name == "Donato's Pizza"
+                    ? require("./imgfiles/donatos.png")
+                    : props.name == "Elliston Place Soda Shop"
+                    ? require("./imgfiles/ellistonplace.png")
+                    : props.name == "Fable Lounge"
+                    ? require("./imgfiles/fable.png")
+                    : props.name == "Frutta Bowls"
+                    ? require("./imgfiles/fruttabowl.png")
+                    : props.name == "Grain & Berry"
+                    ? require("./imgfiles/grainandberry.png")
+                    : props.name == "The Grilled Cheeserie"
+                    ? require("./imgfiles/grilledcheeserie.png")
+                    : props.name == "Helen's Hot Chicken"
+                    ? require("./imgfiles/helenshotchicken.png")
+                    : props.name == "Holy Smokes"
+                    ? require("./imgfiles/holysmokes.png")
+                    : props.name == "Hopdoddy Burger Bar"
+                    ? require("./imgfiles/hopdoddy.png")
+                    : props.name == "I Love Sushi"
+                    ? require("./imgfiles/ilovesushi.png")
+                    : props.name == "Inchin's Bamboo Garden"
+                    ? require("./imgfiles/inchins.png")
+                    : props.name == "Jamba"
+                    ? require("./imgfiles/jamba.png")
+                    : props.name == "Jeni's Splendid Ice Creams"
+                    ? require("./imgfiles/jenis.png")
+                    : props.name == "Jet's Pizza"
+                    ? require("./imgfiles/jetspizza.png")
+                    : props.name == "JP Cafe"
+                    ? require("./imgfiles/jpcafe.png")
+                    : props.name == "Koi Sushi & Thai"
+                    ? require("./imgfiles/koisushi.jpeg")
+                    : props.name == "JP Cafe"
+                    ? require("./imgfiles/jpcafe.png")
+                    : props.name == "Meet Noodles"
+                    ? require("./imgfiles/meetnoodles.png")
+                    : props.name == "Mellow Mushroom"
+                    ? require("./imgfiles/mellowmushroom.png")
+                    : props.name == "Michaelangelo's Pizza"
+                    ? require("./imgfiles/michaelangelo.png")
+                    : props.name == "Nada"
+                    ? require("./imgfiles/nada.png")
+                    : props.name == "Nicoletto's Italian Kitchen"
+                    ? require("./imgfiles/nicolettos.png")
+                    : props.name == "Oscar's Taco Shop"
+                    ? require("./imgfiles/oscarstacoshop.png")
+                    : props.name == "Papa John's Pizza"
+                    ? require("./imgfiles/papajohns.png")
+                    : props.name == "Poke Bros"
+                    ? require("./imgfiles/pokebros.png")
+                    : props.name == "Roma Pizza & Pasta"
+                    ? require("./imgfiles/romapizza.png")
+                    : props.name == "Sarabha's Creamery"
+                    ? require("./imgfiles/sarabhas.png")
+                    : props.name == "Satay Thai Grill"
+                    ? require("./imgfiles/satay.png")
+                    : props.name == "Shokku Ramen"
+                    ? require("./imgfiles/shokkuramen.png")
+                    : props.name == "Sitar Indian Restaurant"
+                    ? require("./imgfiles/sitar.png")
+                    : props.name == "The Slider House"
+                    ? require("./imgfiles/sliderhouse.png")
+                    : props.name == "Sun & Fork"
+                    ? require("./imgfiles/sunandfork.png")
+                    : props.name == "Sweet Dots"
+                    ? require("./imgfiles/sweetdots.png")
+                    : props.name == "Taco Mama"
+                    ? require("./imgfiles/tacomama.png")
+                    : props.name == "Urban Cookhouse"
+                    ? require("./imgfiles/urbancookhouse.png")
+                    : props.name == "The Urban Juicer"
+                    ? require("./imgfiles/urbanjuicer.png")
+                    : props.name == "Wendy's"
+                    ? require("./imgfiles/wendys.png")
+                    : "None"
+                }
+              />
               <div className="restaurantTitle">
                 <b> {props.name} </b>
               </div>
